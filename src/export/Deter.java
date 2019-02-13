ac58e5db6c3dc5e656c000c723b104fa7168be7f
MWintz <35759895+MWintz@users.noreply.github.com>
2019-02-13 02:52:10 +0100

    add question isdeterminist

---
+++ src/export/Deter.java
@@ -0,0 +1,26 @@
+package export;
+
+import automates.Automate;
+
+public class Deter extends Question{
+
+	
+	public Deter(Automate auto) {
+		this.setQtype("Deter");
+		this.setShortanswer(false);
+		this.setQuestion("L'automate donné est-il déterministe ?");
+		this.setReminder("On pourra écrire Vrai, 1, ou True pour Vrai, Faux, 0 ou False pour Faux (attention aux majuscules)");
+		String answer = "{1:SHORTANSWER:";
+		if(auto.isDeterminist() == "Determinist") {
+			answer += "=Vrai~=True~=1~Faux~False~0}";
+		}
+		else {
+			answer += "=Faux~=False~=0~Vrai~True~1}";
+		}
+		
+		this.setAnswer(answer);
+		this.setFeedback("");
+		this.setPenalty(0.3333333);
+		this.setHidden(0);
+	}
+}
