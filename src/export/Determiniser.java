0405111842c08361f7346b26a5949e24b98c378b
MWintz <35759895+MWintz@users.noreply.github.com>
2019-02-24 19:12:09 +0100

    fin xml à tester

--- src/export/Determiniser.java
+++ src/export/Determiniser.java
@@ -12,6 +12,27 @@ public class Determiniser extends Question {
 		this.setReminder("Tous les caractères sont en minuscules, on inscrira x dans les cases qui ne contiennent pas de transition");
 		String answer = " <br><br><table BORDER=\"1\">\n";
 		//TODO Work on the answer
+		auto.determinize();
+		answer += "<TR><TD></TD>\n";
+		for (int i = 0;i<auto.getAlphabet().size();i++) {
+			answer += "<TD>"+auto.getAlphabet().get(i)+"</TD>\n";
+		}
+		answer += "</TR>\n";
+		String[][] table = auto.transitionTable();
+
+		for (int j=1; j<auto.getAutomate().size()+1;j++) {
+			answer += "<TR><TD>"+table[j][0]+"</TD>\n";
+			for (int k=1; k<auto.getAlphabet().size()+1; k++) {
+				if (table[j][k]=="Â°") {
+					answer += "<td>{1:SHORTANSWER:=x}</td>\n";
+				}
+				else {
+					answer += "<td>{1:SHORTANSWER:="+table[j][k]+"}</td>\n";
+				}
+			}
+			answer += "</TR>\n";
+		}
+		
 		/*Analyser l'automate determinise pour pouvoir creer les regles sur chaque case du tableau*/
 		this.setAnswer(answer);
 		this.setFeedback("");
